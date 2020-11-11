b2c73506efa6b9567ef600bb4a7f0b0154bc6e3b
chrissaher <chrissaher@gmail.com>
2020-11-11 10:09:50 +0900

    Improve run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@ IMAGE_NAME=xv6
 IMAGE_TAG=latest
 LOCAL_DIR=$(pwd)/xv6-public
 CONTAINER_DIR=/xv6
-sudo docker run -v $LOCAL_DIR:$CONTAINER_DIR -it $IMAGE_NAME:$IMAGE_TAG qemu-nox
-
+CONTAINER_NAME=/xv6-lab1
+sudo docker run --rm \
+		--name $CONTAINER_NAME \
+		-v $LOCAL_DIR:$CONTAINER_DIR \
+		-it $IMAGE_NAME:$IMAGE_TAG \
+		qemu-nox 
 
