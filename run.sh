223fa6e1ab8bb2587798c997987e32033ce9e13d
Christian Martín Saravia Hernández <chrissaher@gmail.com>
2021-05-08 17:04:09 +0900

    Remove container after exit

--- run.sh
+++ run.sh
@@ -2,6 +2,4 @@ IMAGE_NAME=xv6
 IMAGE_TAG=latest
 LOCAL_DIR=$(pwd)/xv6-public
 CONTAINER_DIR=/xv6
-sudo docker run -v $LOCAL_DIR:$CONTAINER_DIR -it $IMAGE_NAME:$IMAGE_TAG qemu-nox
-
-
+sudo docker run --rm -v $LOCAL_DIR:$CONTAINER_DIR -it $IMAGE_NAME:$IMAGE_TAG qemu-nox
