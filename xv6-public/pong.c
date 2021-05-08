91ddf465de873a021445ed828969d8b059b655ea
Christian Martín Saravia Hernández <chrissaher@gmail.com>
2021-05-08 16:53:48 +0900

    Last commit of lec1

--- xv6-public/pong.c
+++ xv6-public/pong.c
@@ -9,6 +9,21 @@
 int
 main(void)
 {
-  write(1, "hello world\n", 12);
+  int n, pid;
+  int fds[2];
+  char buf[100];
+
+  // create a pipe, with two FDs in fds[0], fds[1].
+  pipe(fds);
+
+  pid = fork();
+  if (pid == 0) {
+    write(fds[1], "this is pipe2\n", 14);
+  } else {
+    wait();
+    n = read(fds[0], buf, sizeof(buf));
+    write(1, buf, n);
+  }
+
   exit();
 }
